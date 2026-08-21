d9d152b27fd2922727fae28e8e01715c2de4d4c4
Mohammad <mqasem784@gmail.com>
2026-08-21 03:18:47 +0300

    [NEW] commit metallica_mis_blobs_9a.h

---
+++ metallica_mis_blobs_9a.h
@@ -0,0 +1,14 @@
+#ifndef __METALLICA_MIS_BLOBS_9A_H
+#define __METALLICA_MIS_BLOBS_9A_H
+
+#include <stddef.h>
+
+/* See metallica_mis_blobs_9a.c for provenance/scope notes. Both are
+ * opaque pre-recorded command blobs for the 06cb:009a sensor only. */
+extern const unsigned char metallica_mis_reset_blob_9a[];
+extern const size_t metallica_mis_reset_blob_9a_len;
+
+extern const unsigned char metallica_mis_db_write_enable_9a[];
+extern const size_t metallica_mis_db_write_enable_9a_len;
+
+#endif /* __METALLICA_MIS_BLOBS_9A_H */
