3b691a8a218c89cebc1afd9910577041c6bf76b7
Mohammad <mqasem784@gmail.com>
2026-09-07 14:35:05 +0300

    Update Build_metallica_mis.sh for new files

--- build_metallica_mis.sh
+++ build_metallica_mis.sh
@@ -35,6 +35,7 @@ fi
 clang metallica_mis_daemon.c metallica_mis_tls.c metallica_mis_init_flash.c \
     metallica_mis_flash.c metallica_mis_blobs_9a.c metallica_mis_firmware.c \
     metallica_mis_upload_fwext.c mmis_rom_info.c \
+    mmis_timeslot.c mmis_calibrate.c mmis_factory_bits.c \
     -o metallica_mis_daemon \
     -I. \
     -I/usr/local/include/libusb-1.0 -L/usr/local/lib -lusb-1.0 \
